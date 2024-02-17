b4f091f9c6cc347d238ec7593f00154eea4a3311
JMcunst <xnslqjtmghaf@gmail.com>
2024-02-17 21:57:03 +0900

    init

---
+++ src/main/java/com/example/discoryservice/DiscoryserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.discoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoryserviceApplication.class, args);
+	}
+
+}
